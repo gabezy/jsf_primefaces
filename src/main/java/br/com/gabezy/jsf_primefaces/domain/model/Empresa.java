e3263632ad4a1136cdb9c59f52e2cc1e4d85dada
gabezy <gabrielmoreira2705@gmail.com>
2025-08-27 10:15:09 -0300

    feat: add Empresa, RamoAtividade entities and TipoEmpresa enumeration

---
+++ src/main/java/br/com/gabezy/jsf_primefaces/domain/model/Empresa.java
@@ -0,0 +1,116 @@
+package br.com.gabezy.jsf_primefaces.domain.model;
+
+import br.com.gabezy.jsf_primefaces.domain.enumaration.TipoEmpresa;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Table(name = "empresa")
+@Entity
+public class Empresa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_fantasia", nullable = false, length = 80)
+    private String nomeFantasia;
+
+    @Column(nullable = false, length = 18, unique = true)
+    private String cnpj;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private TipoEmpresa tipo;
+
+    @Column(name = "razao_social", nullable = false, length = 120)
+    private String razaoSocial;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_fundacao")
+    private Date dataFundacao;
+
+    @ManyToOne
+    @JoinColumn(name = "ramo_atividade_id", nullable = false)
+    private RamoAtividade ramoAtividade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public TipoEmpresa getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoEmpresa tipo) {
+        this.tipo = tipo;
+    }
+
+    public Date getDataFundacao() {
+        return dataFundacao;
+    }
+
+    public void setDataFundacao(Date dataFundacao) {
+        this.dataFundacao = dataFundacao;
+    }
+
+    public RamoAtividade getRamoAtividade() {
+        return ramoAtividade;
+    }
+
+    public void setRamoAtividade(RamoAtividade ramoAtividade) {
+        this.ramoAtividade = ramoAtividade;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" +
+                "nomeFantasia='" + nomeFantasia + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Empresa empresa = (Empresa) o;
+        return Objects.equals(id, empresa.id) && Objects.equals(nomeFantasia, empresa.nomeFantasia) && Objects.equals(razaoSocial, empresa.razaoSocial) && Objects.equals(dataFundacao, empresa.dataFundacao) && Objects.equals(ramoAtividade, empresa.ramoAtividade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nomeFantasia, razaoSocial, dataFundacao, ramoAtividade);
+    }
+}
---
+++ src/main/java/br/com/gabezy/jsf_primefaces/domain/model/RamoAtividade.java
@@ -0,0 +1,54 @@
+package br.com.gabezy.jsf_primefaces.domain.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Table(name = "ramo_atividade")
+@Entity
+public class RamoAtividade implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "RamoAtividade{" +
+                "id=" + id +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        RamoAtividade that = (RamoAtividade) o;
+        return Objects.equals(id, that.id) && Objects.equals(descricao, that.descricao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, descricao);
+    }
+}
